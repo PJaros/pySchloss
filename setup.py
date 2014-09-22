5916c9b0746fcffa2db2abe88b58e2190b4a88a3
Paul Jaros <jaros.paul@gmail.com>
2014-09-22 17:26:02 +0200

    Setup.py hinzugefügt.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pySchloss',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/PJaros/pySchloss',
+    license='Apache License, Version 2.0',
+    author='Paul Jaros',
+    author_email='jaros.paul@gmail.com',
+    description='Programm zum Schlosssystem im Ruum42'
+)
